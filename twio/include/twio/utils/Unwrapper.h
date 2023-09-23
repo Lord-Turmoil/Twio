415d528aede8954c4c4784bd14dbbf5e9334be07
Lord Turmoil <tony-turmoil@qq.com>
2023-09-23 16:47:56 +0800

    update: add header

--- twio/include/twio/utils/Unwrapper.h
+++ twio/include/twio/utils/Unwrapper.h
@@ -1,10 +1,11 @@
+// Copyright (C) 2018 - 2023 Tony's Studio. All rights reserved.
+
 #pragma once
 
 #ifndef _TWIO_UNWRAPPER_H_
 #define _TWIO_UNWRAPPER_H_
 
 #include <twio/Macros.h>
-#include <twio/stream/RedirectRequest.h>
 #include <twio/stream/IStream.h>
 
 TWIO_BEGIN
