31ca287bf35cdda69cd3aa29a215bd27283993be
Lord Turmoil <tony-turmoil@qq.com>
2023-09-23 11:17:58 +0800

    feat: add unwrapper

---
+++ twio/include/twio/utils/Unwrapper.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef _TWIO_UNWRAPPER_H_
+#define _TWIO_UNWRAPPER_H_
+
+#include <twio/Macros.h>
+#include <twio/stream/RedirectRequest.h>
+#include <twio/stream/IStream.h>
+
+TWIO_BEGIN
+
+// A utility class to unwrap stream to raw buffer.
+void UnwrapStream(IOutputStreamPtr stream, char* buffer);
+
+
+TWIO_END
+
+#endif // _TWIO_UNWRAPPER_H_
