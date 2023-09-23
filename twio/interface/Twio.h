b89b0c65d2d1a15ea1d5087c3b9a1bfc3234d72a
Lord Turmoil <tony-turmoil@qq.com>
2023-09-23 16:46:40 +0800

    feat: add all-in-one header

---
+++ twio/interface/Twio.h
@@ -0,0 +1,21 @@
+/*
+ * Common header files for Twio library.
+ */
+
+#ifndef _TWIO_H_
+#define _TWIO_H_
+
+#include <twio/core/Reader.h>
+#include <twio/core/Writer.h>
+#include <twio/core/AdvancedReader.h>
+
+#include <twio/stream/IStream.h>
+#include <twio/stream/BufferInputStream.h>
+#include <twio/stream/BufferOutputStream.h>
+#include <twio/stream/FileInputStream.h>
+#include <twio/stream/FileOutputStream.h>
+
+#include <twio/utils/Printer.h>
+#include <twio/utils/Unwrapper.h>
+
+#endif // _TWIO_H_
